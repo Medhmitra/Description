0f9b1641993098888fa8817713165aa1c45f9282
Medhmitra <medhshan15@gmail.com>
2025-05-26 16:58:09 +0530

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <ul>
+        <li><Link to="/">BasicInfoPage</Link></li>
         <li><Link to="/voucher">Voucher</Link></li>
         <li><Link to="/membership">Membership</Link></li>
         <li><Link to="/promo">Promo Code</Link></li>
